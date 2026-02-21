ca37cc3a4cb7b96c253704e0811c4057c293e228
Zalext <zalext@gmail.com>
2026-02-21 13:09:37 +0100

    Add files via upload

---
+++ 03. Helix ATOM Inner V 0.4 -- Colorear Textos.user.js
@@ -0,0 +1,105 @@
+// ==UserScript==
+// @name         03. INNER ATOM - Colorear Textos | V 0.4
+// @namespace    http://tampermonkey.net/
+// @version      0.4
+// @description  Coloreado dinámico de texto definido en columnas de Inner ATOM (INCs)
+// @author       AL
+// @match        https://atomgencat.onbmc.com/*
+// @run-at       document-idle
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // ----------------------------------------------------
+    // REGLAS DE COLOREADO
+    // ----------------------------------------------------
+
+    const typeColors = {
+        "Customer Communication": 	"red",
+        "Data Acordada": 			"yellow",
+        "Request Information": 		"orange",
+        "Email User": 				"orange",
+        "Company Assignment": 		"blue"
+    };
+
+    const notesColors = {
+        "Assignat a : X03_ARCONTE_GSV": 					"#6e7bf0",
+        "Assignat a : ES_B4_UTE T-SYSTEMS - INDRA": 		"grey",
+        "Assignat a : ES_B4_UTE T-SYSTEMS - INDRA: Text": 	"grey",
+        "AM09_23 - DATA": "brown"
+    };
+
+    const submitterColors = {
+        "46725918S": 				"orange",
+        "44795918S": 				"yellow",
+        "46000918S": 				"yellow",
+        "AR_ESCALATOR": 			"orange",
+        "AM09_23 - DATA": 			"brown"
+    };
+
+    const filesColors = {
+        "1": "red",
+        "2": "red",
+        "3": "red",
+        "4": "red",
+        "5": "red"
+    };
+
+    // ----------------------------------------------------
+    // FUNCIONES AUXILIARES
+    // ----------------------------------------------------
+
+    function colorSpan(span, color) {
+        span.style.color = color;
+        span.style.fontWeight = "bold"; // Revisar a 450
+    }
+
+    function scanSpans(root=document) {
+        root.querySelectorAll("span").forEach(span=>{
+            const text = span.textContent.trim();
+
+            // Type
+            if(typeColors[text]) {
+                colorSpan(span, typeColors[text]);
+            }
+
+            // Notes
+            if(notesColors[text]) {
+                colorSpan(span, notesColors[text]);
+            }
+
+            // Submitter
+            if(submitterColors[text]) {
+                colorSpan(span, submitterColors[text]);
+            }
+
+            // Files
+            if(filesColors[text]) {
+                colorSpan(span, filesColors[text]);
+            }
+        });
+    }
+
+    // ----------------------------------------------------
+    // ESCANEO INICIAL
+    // ----------------------------------------------------
+    scanSpans();
+
+    // ----------------------------------------------------
+    // OBSERVER DINÁMICO
+    // ----------------------------------------------------
+    const observer = new MutationObserver(mutations=>{
+        mutations.forEach(m=>{
+            m.addedNodes.forEach(node=>{
+                if(node.nodeType===1){ // Es elemento
+                    scanSpans(node);
+                }
+            });
+        });
+    });
+
+    observer.observe(document.body, { childList:true, subtree:true });
+
+})();
