b42cfb34119f34c29ce9cf2ca8bce540b92e7e16
Zalext <zalext@gmail.com>
2026-02-21 20:10:53 +0100

    Update 03. Helix ATOM Inner V 0.4 -- Colorear Textos.user.js

--- 03. Helix ATOM Inner V 0.4 -- Colorear Textos.user.js
+++ 03. Helix ATOM Inner V 0.4 -- Colorear Textos.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         03. INNER ATOM - Colorear Textos | V 0.4
+// @name         03. Helix ATOM Inner | V 0.5 - Colorear Textos
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  Coloreado dinámico de texto definido en columnas de Inner ATOM (INCs)
 // @author       AL
 // @match        https://atomgencat.onbmc.com/*
@@ -17,18 +17,30 @@
     // ----------------------------------------------------
 
     const typeColors = {
-        "Customer Communication": 	"red",
-        "Data Acordada": 			"yellow",
+        "General Information": 		"grey",
+        "Customer Communication": 	"yellow",
+        "Data Acordada": 			"red",
         "Request Information": 		"orange",
         "Email User": 				"orange",
-        "Company Assignment": 		"blue"
+        "Company Assignment": 		"blue",
+        "Resolution": 				"green"
     };
 
     const notesColors = {
-        "Assignat a : X03_ARCONTE_GSV": 					"#6e7bf0",
-        "Assignat a : ES_B4_UTE T-SYSTEMS - INDRA": 		"grey",
-        "Assignat a : ES_B4_UTE T-SYSTEMS - INDRA: Text": 	"grey",
-        "AM09_23 - DATA": "brown"
+        "This ticket was created from the service request system.": 	"yellow",
+        "Assignat a : X03_ARCONTE_GSV": 								"#6e7bf0",
+        "Assignat a : ES_B4_UTE T-SYSTEMS - INDRA": 					"grey",
+        "Assignat a : ES_B4_UTE T-SYSTEMS - INDRA: Text": 				"grey",
+        "Assignat a : ESB4-N4-SUPORT PRESENCIAL": 						"grey",
+        "Status has been changed to In Progress": 						"teal",
+        "Status has been changed to Resolved": 							"green",
+        "Assignat a : ES_B4_UTE T-SYSTEMS - INDRA: Text": 				"grey",
+        "AM09_23 - DATA": 												"brown",
+        "Resol. - ": 													"green",
+        "Status has been changed to Completed": 						"green",
+        "Confirmat tancament amb l’usuari.": 							"green",
+        "Status has been changed to Closed": 							"green"
+
     };
 
     const submitterColors = {
@@ -40,6 +52,7 @@
     };
 
     const filesColors = {
+        "0": "grey",
         "1": "red",
         "2": "red",
         "3": "red",
@@ -53,7 +66,7 @@
 
     function colorSpan(span, color) {
         span.style.color = color;
-        span.style.fontWeight = "bold"; // Revisar a 450
+        span.style.fontWeight = "450"; // Revisar a 450
     }
 
     function scanSpans(root=document) {
